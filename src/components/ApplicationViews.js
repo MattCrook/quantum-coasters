91986baeee96161b1c293eb117a2a84ffe7bcd84
Matt Crook <matt.crook11@gmail.com>
2020-03-14 14:42:57 -0500

    file structure update

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -2,7 +2,7 @@
 
 const GetData = async () => {
   const response = await fetch(
-    `https://api.nasa.gov/planetary/apod?api_key=oVbjj1iEMr2fXGT6jgNQugBlL69tPWDaRsZaDbPl`
+    `https://api.nasa.gov/techtransfer?api_key=oVbjj1iEMr2fXGT6jgNQugBlL69tPWDaRsZaDbPl`
   )
     .then(response => response.json())
     .then(data => console.log(data));
