9a8bfe8c9c1a70ca8dcf1e7d4359074d55a5afff
Matt Crook <matt.crook11@gmail.com>
2020-03-18 17:49:36 -0500

    change file name import to creditFORM.

--- src/components/profile/NewCreditForm.js
+++ src/components/profile/NewCreditForm.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import ApiManager from "../../modules/ApiManager";
 import { useAuth0 } from "../../contexts/react-auth0-context";
-import "./NewCreditFrom.css";
+import "./NewCreditForm.css";
 
 const AddNewCreditForm = props => {
   const { loading, user, history } = useAuth0();
