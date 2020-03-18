2786a2652988dacd6be6cebd2045cf453debdcf6
Matt Crook <matt.crook11@gmail.com>
2020-03-18 12:24:23 -0500

    Profile page (#3)
    * fix: add db back. Profile list and card.
    
    * feat: profile page routed. Todo: get corect user and picture.
    
    * refactor: refactor auth0-context folder to use hooks instead of classes.
    
    * fix: delete account directory.
    
    * feat: profile and auth update. Messages started.
    
    * WIP: to do: route user to prfile to complete before sending back to home.
    
    * Feat: profile list update to route to form. Add new rollercoaster form in progress.
    
    * wip: add new form html structure skeloton created. Todo: created button on profile page to take user to form.
    
    * feat: new file NewCreditForm. Will give user abiity to add a credit. If info does not exist they will have the ability to fill out NewRollerCoasterForm then go back and record the credit.

---
+++ src/components/addNewForm/NewRollerCoaster.js
@@ -0,0 +1,133 @@
+import React, { useState } from "react";
+import ApiManager from "../../modules/ApiManager";
+import { useAuth0 } from "../../contexts/react-auth0-context";
+
+const AddNewRollerCoaster = (props) => {
+  const { loading, user, history } = useAuth0();
+
+  // set initial state of form to empty
+  const [rollerCoaster, setRollerCoaster] = useState({
+    name: "",
+    trackType: "",
+    max_height: "",
+    max_speed: "",
+    park: "",
+    manufacturer: "",
+    userId: ""
+  });
+
+  // handle what user is typing in
+  const handleFieldChange = e => {
+    const stateToChange = { ...rollerCoaster };
+    stateToChange[e.target.id] = e.target.value;
+    setRollerCoaster(stateToChange);
+  };
+
+  // object to go into database
+  const constructNewRollerCoaster = e => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    const newRollerCoaster = {
+      name: rollerCoaster.name,
+      trackType: rollerCoaster.trackType,
+      max_height: rollerCoaster.max_height,
+      max_speed: rollerCoaster.max_speed,
+      park: rollerCoaster.park,
+      manufacturer: rollerCoaster.manufacturer,
+      userId: user.id
+    };
+
+    // guard to make sure all fields are filled out
+    if (
+      rollerCoaster.name === "" ||
+      rollerCoaster.trackType === "" ||
+      rollerCoaster.max_height === "" ||
+      rollerCoaster.max_speed === "" ||
+      rollerCoaster.manufacturer === "" ||
+      rollerCoaster.park === ""
+    ) {
+      alert("Please fill out all fields in form");
+    } else {
+      !loading &&
+        ApiManager.postNewRollerCoaster(newRollerCoaster).then(() =>
+          history.push("/profile")
+        );
+    }
+  };
+
+  return (
+    <form className="main-form" onSubmit={constructNewRollerCoaster}>
+      <fieldset className="fs-form">
+        <h3 className="title">Input Ride Details</h3>
+        <div className="create-form">
+          <label htmlFor="inputName">Roller Coaster Name</label>
+          <input
+            className="input"
+            onChange={handleFieldChange}
+            type="name"
+            id="name"
+            placeholder="Enter Roller Coaster Name"
+            required=""
+            autoFocus=""
+          />
+          <label htmlFor="inputTrackType">Track Type</label>
+          <input
+            className="input"
+            onChange={handleFieldChange}
+            type="dropdown"
+            id="trackType"
+            placeholder="Select Track Type"
+            required=""
+            autoFocus=""
+          />
+
+          <label htmlFor="inputPassword">Max Height</label>
+          <input
+            className="input"
+            onChange={handleFieldChange}
+            type="text"
+            id="max_height"
+            placeholder="Max Height"
+            required=""
+            autoFocus=""
+          />
+          <label htmlFor="confirm-password">Max Speed</label>
+          <input
+            className="input"
+            onChange={handleFieldChange}
+            type="text"
+            id="max_speed"
+            placeholder="Max Speed"
+            required=""
+            autoFocus=""
+          />
+          <label htmlFor="confirm-password">Home Park</label>
+          <input
+            className="input"
+            onChange={handleFieldChange}
+            type="text"
+            id="park"
+            placeholder="Enter Home Park Of Ride"
+            required=""
+            autoFocus=""
+          />
+          <input
+            className="input"
+            onChange={handleFieldChange}
+            type="dropdown"
+            id="park"
+            placeholder="Select Manufacturer"
+            required=""
+            autoFocus=""
+          />
+          <button className="create-btn" type="submit">
+            Submit
+          </button>
+        </div>
+      </fieldset>
+    </form>
+  );
+};
+
+export default AddNewRollerCoaster;
---
+++ src/components/profile/NewCreditForm.js
@@ -0,0 +1,4 @@
+import React, { useState, useEffect } from "react";
+import ProfileCard from "./ProfileCard";
+import ApiManager from "../../modules/ApiManager";
+import { useAuth0 } from "../../contexts/react-auth0-context";
