f83a0d3f13fcc3a24248d0ca520980cc76f81bea
Matt Crook <matt.crook11@gmail.com>
2020-03-19 15:50:28 -0500

    feat: refactor: some logic extracted to helper.js. MEssages components logic in progress. Todo: check passing props vs pssing the object. Needs a second look. (#6)

--- src/components/messages/MessageForm.js
+++ src/components/messages/MessageForm.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import ApiManager from "../../modules/ApiManager";
-import isEditCheck from "../../modules/Helpers";
+import { isEditCheck, handleSubmitHelperFunction } from "../../modules/Helpers";
 import { useAuth0 } from "../../contexts/react-auth0-context";
 
 const MessageForm = props => {
@@ -29,6 +29,8 @@ const MessageForm = props => {
       // and the timestamp should be what it was.
       // userId doesn't need to change because users
       // will not have a button to edit other users' messages
+
+      // this function was extracted to module scope (in helpers directory)
       return isEditCheck(props, messageToPost);
     }
   };
@@ -37,10 +39,66 @@ const MessageForm = props => {
     // If the object has an id, it is an edit
     // so we put/update
     if (message.hasOwnProperty("id")) {
-      return ApiManager.updatePut("messages", message);
+      return ApiManager.updateMessagesPut(message);
     } else {
       // Otherwise, it is new, so we post
-      return ApiManager.post("messages", message);
+      return ApiManager.postMessage(message);
     }
   };
+
+
+  const handleSubmit = handleSubmitHelperFunction(
+    setLoading,
+    constructNewMessage,
+    props,
+    postEditedMessage
+  );
+
+
+  useEffect(() => {
+    if (props.messageToEdit.message) {
+      setMessage(props.messageToEdit);
+      // A "one-time" setting of the message field's value
+      // when there's a new props.messageToEdit containing a message string
+      // (which happens when the edit button is clicked)
+      document.getElementById("message").value = props.messageToEdit.message;
+    }
+    setLoading(false);
+  }, [props.messageToEdit, setLoading]); // not sure about the [setLoading]
+
+  return (
+    <>
+      {/*
+      https://stackoverflow.com/a/33212911
+      Form Submit allows the Enter key to work 
+      instead of just clicking the send button
+      */}
+      <div className="message-container, darker, chat-input-container">
+        <form className="chat-form" onSubmit={handleSubmit}>
+          <fieldset className="chat-fieldset">
+            <div className="chat-input-inner-container">
+              <input
+                className="chat-input"
+                name="message"
+                type="message"
+                required
+                onChange={handleFieldChange}
+                id="message"
+                placeholder="Chat message"
+              />
+              <button className="chat-send-button" data-tooltip="SEND">
+                <i
+                  className="paper plane outline icon"
+                  type="submit"
+                  disabled={loading}
+                ></i>
+              </button>
+            </div>
+          </fieldset>
+        </form>
+      </div>
+    </>
+  );
 };
+
+export default MessageForm;
