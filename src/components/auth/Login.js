7781c7c93907a9c6208a53b2564905d8d34fb14a
Matt Crook <matt.crook11@gmail.com>
2020-05-28 10:42:11 -0500

    fix: took out login ID on Login/auth component. Messed up CSS.

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -22,7 +22,7 @@ const LoginLandingPage = () => {
             <div className="navbar-end">
               {/* if there is no user. show the login button */}
               {!loading && !user && (
-                <button onClick={loginWithRedirect} className="navbar-item" id="login">
+                <button onClick={loginWithRedirect} className="navbar-item">
                   Login
                 </button>
               )}
