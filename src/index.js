f4608460817e3a7c7e7a8e136b4ebd1ee4dcd07b
Matt Crook <matt.crook11@gmail.com>
2020-11-20 11:01:19 -0600

    feat: higher order component for auth0provider and history.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
-import { Auth0Provider } from "./contexts/react-auth0-context";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
-import config from "./auth_config.json";
-import history from "./utils/history";
+import Auth0ProviderWithHistory from "./components/auth/auht0ProviderWithHistory";
+// import { Auth0Provider } from "./contexts/react-auth0-context";
+
+// import config from "./auth_config.json";
+// import history from "./utils/history";
+
 // import configureStore from './store'
 // import createHistory from 'history/createBrowserHistory'
 // import { ConnectedRouter } from 'react-router-redux'
@@ -13,29 +17,33 @@ import history from "./utils/history";
 // const history = createHistory()
 // const store = configureStore(history)
 
-
-
 // A function that routes the user to the right place after login
-const onRedirectCallback = appState => {
-  history.push(
-    appState && appState.targetUrl
-    ? appState.targetUrl
-    : window.location.pathname
-    );
-};
+// const onRedirectCallback = (appState) => {
+//   history.push(appState && appState.targetUrl ? appState.targetUrl : window.location.pathname);
+// };
 
+// ReactDOM.render(
+//   <BrowserRouter>
+//     <Auth0Provider
+//       domain={config.domain}
+//       client_id={config.client_id}
+//       redirect_uri={`${window.location.origin}/home`}
+//       onRedirectCallback={onRedirectCallback}
+//       audience={config.audience}
+//       scope={config.scope}
+//     >
+//       <App />
+//     </Auth0Provider>
+//   </BrowserRouter>,
+//   document.getElementById("root")
+// );
 
 ReactDOM.render(
-  <Auth0Provider
-    domain={config.domain}
-    client_id={config.client_id}
-    redirect_uri={`${window.location.origin}/home`}
-    onRedirectCallback={onRedirectCallback}
-    audience={config.audience}
-    scope={config.scope}
-  >
-    <App />
-  </Auth0Provider>,
+  <Router>
+    <Auth0ProviderWithHistory>
+      <App />
+    </Auth0ProviderWithHistory>
+  </Router>,
   document.getElementById("root")
 );
 
