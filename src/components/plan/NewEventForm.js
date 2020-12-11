b7803e646fa62c30c3009887e354745f7ff7a7ad
Matt Crook <matt.crook11@gmail.com>
2020-12-11 17:58:09 -0600

    wip: calendar styles and updates.

--- src/components/plan/NewEventForm.js
+++ src/components/plan/NewEventForm.js
@@ -1,11 +1,42 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Reminders from "./Reminders";
+import calendarManager from "../../modules/calendar/calendarManager";
+import TimePicker from "./Pickers";
+import Grid from "@material-ui/core/Grid";
+import DateFnsUtils from "@date-io/date-fns";
+import { MuiPickersUtilsProvider, KeyboardTimePicker, KeyboardDatePicker } from "@material-ui/pickers";
 import "./Plan.css";
 
 const NewEventForm = (props) => {
-  const [isReminder, setIsReminder] = useState(false);
+  const [addReminder, setAddReminder] = useState(false);
+  const [title, setTitle] = useState();
+  const [location, setLocation] = useState();
+  const [description, setDescription] = useState();
+  const [startTime, setStartTime] = useState();
+  const [endTime, setEndTime] = useState();
+  const [reminderValue, setReminderValue] = useState(null);
+  const [isReminderSet, setIsReminderSet] = useState(false);
+
   const currentDate = sessionStorage.getItem("CalendarDateIsSelected");
-//   console.log(isReminder);
+
+  const handleEventFormSubmit = (e) => {
+    e.preventDefault();
+    const newEvent = {
+      title: title,
+      location: location,
+      description: description,
+      startTime: startTime,
+      endTime: endTime,
+      reminderValue: reminderValue,
+      isReminderSet: isReminderSet,
+    };
+    calendarManager
+      .postUserCalendarEvent(newEvent)
+      .then(() => {
+        // props.history.push("/plan");
+      })
+      .catch((err) => console.log(err));
+  };
 
   return (
     <>
@@ -18,36 +49,80 @@ const NewEventForm = (props) => {
       </nav>
       <div className="add_event_form_container">
         <div className="event_form_title">Add Calendar Event</div>
-        <form className="add_event_form">
+        <form className="add_event_form" onSubmit={handleEventFormSubmit}>
           <div className="event_form_header">
             <div className="current_date_event_form">{currentDate}</div>
-            <div className="cancel" onClick={() => props.history.push("/plan")}>Cancel</div>
+            <div className="cancel" onClick={() => props.history.push("/plan")}>
+              Cancel
+            </div>
           </div>
           <div className="row_wrapper">
             <label className="event_form_label" htmlFor="title">
               Title:
             </label>
-            <input className="event_form_input" id="title" />
+            <input className="event_form_input" id="title" onChange={(e) => setTitle(e.target.value)} />
           </div>
 
           <div className="row_wrapper">
             <label className="event_form_label" htmlFor="location">
               Location:
             </label>
-            <input className="event_form_input" id="title" />
+            <input className="event_form_input" id="location" onChange={(e) => setLocation(e.target.value)} />
           </div>
 
           <div className="row_wrapper">
             <label className="event_form_label" htmlFor="description">
               Description:
             </label>
-            <input className="event_form_input" id="title" />
+            <input className="event_form_input" id="description" onChange={(e) => setDescription(e.target.value)} />
           </div>
-          <div className="row_wrapper">
-            <label className="event_form_label" htmlFor="time">
+          <div id="start_end_row_wrapper">
+            <label className="start_end_event_form_label" htmlFor="time">
               Time:
             </label>
-            <input className="event_form_input" id="title" />
+            <div id="start_end_time">
+              <div className="start">
+                Start:
+                <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                  <Grid container justify="space-around">
+                    <KeyboardTimePicker
+                      margin="normal"
+                      id="time-picker"
+                      label="Start Time"
+                      value={startTime}
+                      onChange={(e) => setStartTime(e.target.value)}
+                      KeyboardButtonProps={{
+                        "aria-label": "change time",
+                      }}
+                    />
+                  </Grid>
+                </MuiPickersUtilsProvider>
+                {/* <input className="event_form_input" id="time" type="time" onChange={(e) => setStartTime(e.target.value)} /> */}
+              </div>
+              <div className="end">
+                End:
+                <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                  <Grid container justify="space-around">
+                    <KeyboardTimePicker
+                      margin="normal"
+                      id="time-picker"
+                      label="End Time"
+                      value={startTime}
+                      onChange={(e) => setEndTime(e.target.value)}
+                      KeyboardButtonProps={{
+                        "aria-label": "change time",
+                      }}
+                    />
+                  </Grid>
+                </MuiPickersUtilsProvider>
+                {/* <input
+                  className="event_form_input"
+                  id="time"
+                  type="time"
+                  onChange={(e) => setEndTime(e.target.value)}
+                /> */}
+              </div>
+            </div>
           </div>
 
           <div id="master_reminders_container">
@@ -61,7 +136,8 @@ const NewEventForm = (props) => {
                       id="no_reminders"
                       name="is_reminder_set"
                       value="False"
-                      onClick={() => setIsReminder(false)}
+                      onClick={() => setAddReminder(false)}
+                      onChange={(e) => setIsReminderSet(e.target.value)}
                     />
                     <label htmlFor="no_reminders">No Reminders</label>
                     <div className="check"></div>
@@ -72,7 +148,8 @@ const NewEventForm = (props) => {
                       id="reminders"
                       name="is_reminder_set"
                       value="True"
-                      onClick={() => setIsReminder(true)}
+                      onClick={() => setAddReminder(true)}
+                      onChange={(e) => setIsReminderSet(e.target.value)}
                     />
                     <label htmlFor="reminders">Set A Reminder</label>
                     <div className="check"></div>
@@ -80,9 +157,27 @@ const NewEventForm = (props) => {
                 </ul>
               </div>
             </div>
-            <div id="reminders_container_toggles">
-              <Reminders authUser={props.authUser} userProfile={props.userProfile} {...props} />
-            </div>
+            {addReminder ? (
+              <div id="reminders_container_toggles">
+                <Reminders
+                  authUser={props.authUser}
+                  userProfile={props.userProfile}
+                  addReminder={addReminder}
+                  setReminderValue={setReminderValue}
+                  {...props}
+                />
+              </div>
+            ) : (
+              <div id="reminders_container_toggles" style={{ opacity: "0.2" }}>
+                <Reminders
+                  authUser={props.authUser}
+                  userProfile={props.userProfile}
+                  addReminder={addReminder}
+                  setReminderValue={setReminderValue}
+                  {...props}
+                />
+              </div>
+            )}
           </div>
 
           <button id="new_event_cal_save_btn" type="submit">
@@ -90,7 +185,7 @@ const NewEventForm = (props) => {
           </button>
         </form>
       </div>
-      <div className="signature">
+      <div id="sig_new_event_form" className="signature">
         <p id="signature_font">
           Made by <a href="https://matt-crook-io.now.sh/">Quantum Coasters</a> <i className="fas fa-trademark"></i>
         </p>
--- src/components/plan/Reminders.js
+++ src/components/plan/Reminders.js
@@ -7,17 +7,19 @@ const Reminders = (props) => {
     const oneHourSwitch = useRef();
 
 
-  const handleToggleSwitch = (e, themeSwitch) => {
+  const handleToggleSwitch = (e, themeSwitch, reminderValue) => {
     e.preventDefault();
-    themeSwitch.current.classList.toggle("light-theme");
+      themeSwitch.current.classList.toggle("light-theme");
+      props.setReminderValue(reminderValue);
   };
     
+
   return (
     <div className="reminders_container">
       <div className="reminder_switch_container">
         <div className="reminder_switch_title">1 Week Before</div>
         <div className="reminder_options">
-          <div ref={oneWeekSwitch} className="theme-switch" onClick={(e) => handleToggleSwitch(e, oneWeekSwitch)}>
+          <div ref={oneWeekSwitch} className="theme-switch" onClick={(e) => handleToggleSwitch(e, oneWeekSwitch, "1 Week")}>
             <div className="switch"></div>
           </div>
         </div>
@@ -26,7 +28,7 @@ const Reminders = (props) => {
       <div className="reminder_switch_container">
         <div className="reminder_switch_title">1 Day Before</div>
         <div className="reminder_options">
-          <div ref={oneDaySwitch} className="theme-switch" onClick={(e) => handleToggleSwitch(e, oneDaySwitch)}>
+          <div ref={oneDaySwitch} className="theme-switch" onClick={(e) => handleToggleSwitch(e, oneDaySwitch, "1 Day")}>
             <div className="switch"></div>
           </div>
         </div>
@@ -35,7 +37,7 @@ const Reminders = (props) => {
       <div className="reminder_switch_container">
         <div className="reminder_switch_title">1 Hour Before</div>
         <div className="reminder_options">
-          <div ref={oneHourSwitch} className="theme-switch" onClick={(e) => handleToggleSwitch(e, oneHourSwitch)}>
+          <div ref={oneHourSwitch} className="theme-switch" onClick={(e) => handleToggleSwitch(e, oneHourSwitch, "1 Hour")}>
             <div className="switch"></div>
           </div>
         </div>
