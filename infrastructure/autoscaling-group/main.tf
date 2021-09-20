2fbddb418d72a07abb5c978b3910ebf26863bb59
Matt Crook <matt.crook11@gmail.com>
2021-09-20 17:48:50 -0500

    fix: networking for external lb.

--- infrastructure/autoscaling-group/main.tf
+++ infrastructure/autoscaling-group/main.tf
@@ -77,117 +77,11 @@ module "managed_instance_autoscaling_group" {
   project                      = "${var.project}"
   region                       = "us-central1"
   compute_resource_policy_name = "quantum-coasters-daily-backup"
-  can_ip_forward               = false
+  can_ip_forward               = true
   docker_username              = "${file("${path.module}/docker.username")}"
   docker_password              = "${file("${path.module}/docker.password")}"
   private_key                  = "${file("${path.module}/tls.key")}"
   certificate                  = "${file("${path.module}/tls.cert")}"
   autoscaler_name              = "quantum-coasters-autoscaler"
+  https_proxy_name             = "https-proxy"
 }
-
-// resource "google_compute_firewall" "allow_all" {
-//   project     = "${var.project}"
-//   name        = "ingress-allow-all"
-//   network     = "${data.google_compute_network.default_network.name}"
-//   direction   = "INGRESS"
-//   description = "Firewall rule allows all traffic with protocols tcp, udp, and icmp from all IPs into port 80 and port 443."
-
-//   source_ranges = "${local.all_ips}"
-
-//   allow {
-//     protocol = "${local.tcp_protocol}"
-//     ports    = ["80", "443"]
-//   }
-
-//   allow {
-//     protocol = "icmp"
-//   }
-
-//   allow {
-//     protocol = "udp"
-//   }
-
-//   log_config {
-//     metadata = "EXCLUDE_ALL_METADATA"
-//   }
-// }
-
-// resource "google_compute_firewall" "egress_allow_all" {
-//   project     = "${var.project}"
-//   name        = "egress-allow-all"
-//   network     = "${data.google_compute_network.default_network.name}"
-//   direction   = "EGRESS"
-//   # priority    = 65535
-//   priority    = 1000
-//   description = "Firewall rule allows all Egress traffic from all IPs and all ports."
-
-
-//   allow {
-//     protocol = "all"
-//   }
-
-//   log_config {
-//     metadata = "EXCLUDE_ALL_METADATA"
-//   }
-// }
-
-
-// resource "google_compute_firewall" "allow_http" {
-//   project     = "${var.project}"
-//   name        = "ingress-allow-http"
-//   network     = "${data.google_compute_network.default_network.name}"
-//   direction   = "INGRESS"
-//   description = "Firewall rule allows Ingress traffic on port 80 into VM instances with tag quantum-coasters-production."
-
-//   # source_tags = ["quantum-coasters-production"]
-//   target_tags = ["quantum-coasters-production"]
-
-//   allow {
-//     protocol = "${local.tcp_protocol}"
-//     ports    = ["${var.server_port}"]
-//   }
-
-//   log_config {
-//     metadata = "EXCLUDE_ALL_METADATA"
-//   }
-// }
-
-// resource "google_compute_firewall" "allow_https" {
-//   project     = "${var.project}"
-//   name        = "ingress-allow-https"
-//   network     = "${data.google_compute_network.default_network.name}"
-//   direction   = "INGRESS"
-//   description = "Firewall rule allows Ingress traffic on port 443 into VM instances with tag quantum-coasters-production."
-
-//   # source_tags = ["quantum-coasters-production"]
-//   target_tags = ["quantum-coasters-production"]
-
-//   allow {
-//     protocol = "${local.tcp_protocol}"
-//     ports    = ["443"]
-//   }
-
-//   log_config {
-//     metadata = "EXCLUDE_ALL_METADATA"
-//   }
-// }
-
-// resource "google_compute_firewall" "allow_ssh" {
-//   project     = "${var.project}"
-//   name        = "ingress-allow-https"
-//   network     = "${data.google_compute_network.default_network.name}"
-//   direction   = "INGRESS"
-//   priority    = 1000
-//   description = "Firewall rule allows Ingress traffic for SSH on port 22 into VM instances with tag quantum-coasters-production."
-
-//   target_tags = ["quantum-coasters-production"]
-
-//   allow {
-//     protocol = "tcp"
-//     ports    = ["22"]
-//   }
-
-//   log_config {
-//     metadata = "EXCLUDE_ALL_METADATA"
-//   }
-// }
